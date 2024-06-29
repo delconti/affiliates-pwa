b3ec9e74cb0299c89c1bfaac078458507ab26afd
delconti <henrique.delconti@gmail.com>
2024-06-29 17:13:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ registrationForm.addEventListener('submit', async (event) => {
     
     const formData = new FormData(registrationForm);
     const data = {
+        name: formData.get('name'),
         email: formData.get('email'),
         phone: formData.get('phone'),
         affiliateId: formData.get('affiliateId'),
@@ -22,19 +23,33 @@ registrationForm.addEventListener('submit', async (event) => {
             allowLocalhostAsSecureOrigin: true,
         });
 
-        OneSignal.push(() => {
-            OneSignal.getUserId(async (userId) => {
-                if (userId) {
-                    // Set external user ID
-                    OneSignal.setExternalUserId(data.affiliateId);
-                    
-                    // Save essential user information to OneSignal tags
-                    await OneSignal.sendTags({
+        OneSignal.push(async () => {
+            await OneSignal.setExternalUserId(data.affiliateId);
+
+            // Save essential user information to OneSignal tags
+            await OneSignal.sendTags({
+                name: data.name
+            });
+
+            // Use OneSignal API to store additional user data
+            const response = await fetch('https://onesignal.com/api/v1/players', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    'Authorization': `Basic MjQ0Yzk0MTItNGNjMC00ZDU0LTg1N2EtZDlkYTI2ZTc5NTYy`, // Substitua pela sua API Key
+                },
+                body: JSON.stringify({
+                    app_id: "a7baa9a1-f62e-4a3f-8933-895f85ae1154", // Substitua por sua App ID
+                    identifier: data.affiliateId,
+                    tags: {
                         email: data.email,
                         phone: data.phone
-                    });
-                }
+                    }
+                })
             });
+
+            const result = await response.json();
+            console.log(result);
         });
     });
 });
